e7d97084db80d6b6b773f71566bae060159c1fd5
woldtwerk <woldt.willi@gmail.com>
2025-08-20 08:24:51 +0200

    feat: :sparkles: 28_find-the-index-of-the-first-occurrence-in-a-string

---
+++ problems/easy/28_find-the-index-of-the-first-occurrence-in-a-string/index.ts
@@ -0,0 +1,7 @@
+/*
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+*/
+
+export function strStr(haystack: string, needle: string): number {
+  return haystack.indexOf(needle)
+}
