d266fb51e03bd585850e0967614962c9037cdd15
woldtwerk <woldt.willi@gmail.com>
2025-08-26 18:46:39 +0200

    feat: :sparkles: 69_sqrt

---
+++ utils.ts
@@ -0,0 +1,22 @@
+export class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val
+    this.next = next === undefined ? null : next
+  }
+}
+
+export function array2LinkedList(values: number[]): ListNode | null {
+  if (!values || values.length === 0) return null;
+
+  const head = new ListNode(values[0]);
+  let current = head;
+
+  for (let i = 1; i < values.length; i++) {
+    current.next = new ListNode(values[i]);
+    current = current.next;
+  }
+
+  return head;
+}
